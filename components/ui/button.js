9ff147dca46e55916a91336fbed7a6b442d17959
Katerina Popova <katerina_26popova@yahoo.com>
2023-10-11 23:51:53 +0200

    routing project finished

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,11 +2,22 @@ import Link from "next/link";
 
 import classes from "./button.module.css";
 
+// This button component should either render a link or a normal button.
+//And it should render a link if the prop is set, if is not set it should render a norlam button
+// so we need to check
+
 function Button(props) {
+  if (props.link) {
+    return (
+      <Link href={props.link} className={classes.btn}>
+        {props.children}
+      </Link>
+    );
+  }
   return (
-    <Link href={props.link} className={classes.btn}>
+    <button className={classes.btn} onClick={props.onClick}>
       {props.children}
-    </Link>
+    </button>
   );
 }
 export default Button;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventList from "../../components/events/event-list";
+import { getAllEvents } from "../../dummy-data";
+import EventsSearch from "../../components/events/events-search";
 
 function AllEventsPage() {
+  const router = useRouter();
+  const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+
   return (
-    <div>
-      <h1> The All Events Page</h1>
-    </div>
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
   );
 }
 
